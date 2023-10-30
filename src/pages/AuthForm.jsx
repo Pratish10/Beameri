96b9794c13050a024e1200598d1964cfd08c0eb4
Pratish Ninawe <pratish.1006@gmail.com>
2023-10-30 17:09:12 +0530

    feat: implemented login and register api

--- src/pages/AuthForm.jsx
+++ src/pages/AuthForm.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import {
+  Alert,
   Box,
   Button,
   FormControl,
@@ -9,16 +10,38 @@ import {
   InputLabel,
   OutlinedInput,
   Paper,
+  Snackbar,
   TextField,
   Typography,
 } from "@mui/material";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 const AuthForm = () => {
   const [variant, setVariant] = React.useState("LOGIN");
   const [isLoading, setIsLoading] = React.useState(false);
   const [showPassword, setShowPassword] = React.useState(false);
+
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [confirmPassword, setConfirmPassword] = React.useState("");
+
+  const [nameError, setNameError] = React.useState("");
+  const [emailError, setEmailError] = React.useState("");
+  const [passwordError, setPasswordError] = React.useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = React.useState("");
+
+  const [snackbar, setSnackbar] = React.useState({
+    open: false,
+    severity: "error",
+    message: "",
+  });
+
+  const navigate = useNavigate();
 
   const commonStyles = {
     fullWidth: { fullWidth: true },
@@ -39,19 +62,20 @@ const AuthForm = () => {
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
-  const toggleVariantHandler = () => {
+  const toggleVariantHandler = React.useCallback(() => {
     setVariant((prevVariant) =>
       prevVariant === "LOGIN" ? "REGISTER" : "LOGIN"
     );
-  };
+  }, []);
 
-  const renderTextField = (label, type, id) => (
+  const renderTextField = (label, type, id, setState) => (
     <Grid item xs={12} sx={{ py: 1 }}>
       <TextField
         variant="outlined"
         label={label}
         type={type}
         id={id}
+        onChange={(e) => setState(e.target.value)}
         {...commonStyles.fullWidth}
         sx={commonStyles.inputContainer}
       />
@@ -69,6 +93,7 @@ const AuthForm = () => {
         <OutlinedInput
           id="password"
           type={showPassword ? "text" : "password"}
+          onChange={(e) => setPassword(e.target.value)}
           endAdornment={
             <InputAdornment position="end">
               <IconButton onClick={handleClickShowPassword} edge="end">
@@ -93,6 +118,7 @@ const AuthForm = () => {
         <OutlinedInput
           id="confirmPassword"
           type={showPassword ? "text" : "password"}
+          onChange={(e) => setConfirmPassword(e.target.value)}
           endAdornment={
             <InputAdornment position="end">
               <IconButton onClick={handleClickShowPassword} edge="end">
@@ -106,19 +132,113 @@ const AuthForm = () => {
     </Grid>
   );
 
-  const submitHandler = (event) => {
+  const closeSnackbar = () => {
+    setSnackbar({ ...snackbar, open: false });
+  };
+
+  const submitHandler = async (event) => {
     event.preventDefault();
     setIsLoading(true);
 
     if (variant === "LOGIN") {
-      console.log("LOGIN");
+      if (!email || !password) {
+        setEmailError(!email ? "Email is required" : "");
+        setPasswordError(!password ? "Password is required" : "");
+        setIsLoading(false);
+        setSnackbar({
+          open: true,
+          severity: "error",
+          message: "Email and password are required.",
+        });
+        return;
+      }
+      try {
+        const response = await axios.post(
+          `https://crowded-sweatpants-elk.cyclic.cloud/api/customer/login`,
+          {
+            email,
+            password,
+          }
+        );
+        if (response.status === 200) {
+          setSnackbar({
+            open: true,
+            severity: "success",
+            message: "Login successful.",
+          });
+          navigate("/");
+        }
+      } catch (error) {
+        console.log(error);
+        setSnackbar({
+          open: true,
+          severity: "error",
+          message: error.message,
+        });
+      } finally {
+        setIsLoading(false);
+      }
     }
 
     if (variant === "REGISTER") {
-      console.log("REGISTER");
+      if (!name || !email || !password || !confirmPassword) {
+        setNameError(!name ? "Name is required" : "");
+        setEmailError(!email ? "Email is required" : "");
+        setPasswordError(!password ? "Password is required" : "");
+        setConfirmPasswordError(
+          !confirmPassword ? "Confirm Password is required" : ""
+        );
+        setIsLoading(false);
+        setSnackbar({
+          open: true,
+          severity: "error",
+          message: "All fields are required for registration.",
+        });
+
+        return;
+      }
+
+      if (password !== confirmPassword) {
+        setConfirmPasswordError("Password and Confirm Password must match");
+        setIsLoading(false);
+        setSnackbar({
+          open: true,
+          severity: "error",
+          message: "Password and Confirm Password do not match.",
+        });
+        return;
+      }
+
+      try {
+        const response = await axios.post(
+          `https://crowded-sweatpants-elk.cyclic.cloud/api/customer/register`,
+          {
+            name,
+            email,
+            password,
+          }
+        );
+        if (response.status === 201) {
+          console.log("Registration successful.");
+          setSnackbar({
+            open: true,
+            severity: "success",
+            message: "Registration successful.",
+          });
+          navigate("/");
+        }
+      } catch (error) {
+        console.log(error.message);
+        setSnackbar({
+          open: true,
+          severity: "error",
+          message: error.message,
+        });
+      } finally {
+        setIsLoading(false);
+      }
     }
   };
-
   return (
     <div style={{ marginTop: "10rem" }}>
       <Paper
@@ -145,14 +265,63 @@ const AuthForm = () => {
         >
           {variant === "LOGIN" ? "Log In" : "Register"}
         </Box>
+        {emailError && (
+          <p
+            style={{
+              color: "red",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {emailError}
+          </p>
+        )}
+        {passwordError && (
+          <p
+            style={{
+              color: "red",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {passwordError}
+          </p>
+        )}
+        {nameError && (
+          <p
+            style={{
+              color: "red",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {nameError}
+          </p>
+        )}
+        {confirmPasswordError && (
+          <p
+            style={{
+              color: "red",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {confirmPasswordError}
+          </p>
+        )}
         <Box sx={{ px: 3, py: 4, mt: 3 }}>
           <form onSubmit={submitHandler}>
             <Grid container spacing={2}>
               {variant === "REGISTER" &&
-                renderTextField("Name", "text", "name")}
-              {renderTextField("Email", "email", "email")}
-              {renderPasswordInput()}
-              {variant === "REGISTER" && renderConfirmPasswordInput()}
+                renderTextField("Name", "text", "name", setName, nameError)}
+              {renderTextField("Email", "email", "email", setEmail, emailError)}
+              {renderPasswordInput(passwordError)}
+              {variant === "REGISTER" &&
+                renderConfirmPasswordInput(confirmPasswordError)}
 
               {variant === "LOGIN" && (
                 <Typography
@@ -189,6 +358,7 @@ const AuthForm = () => {
                     borderRadius: "1.875rem",
                   }}
                 >
+                  {isLoading ? "Loading        " : ""}
                   {variant === "LOGIN" ? "Log In" : "Register"}
                 </Button>
               </Grid>
@@ -229,6 +399,16 @@ const AuthForm = () => {
           </div>
         </Box>
       </Paper>
+      <Snackbar
+        open={snackbar.open}
+        autoHideDuration={6000}
+        onClose={closeSnackbar}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      >
+        <Alert severity={snackbar.severity} onClose={closeSnackbar}>
+          {snackbar.message}
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
